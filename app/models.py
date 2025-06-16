c759c0b759e6ce551a46f2beedb3d3adaa24e7d4
Forlating <dun638691@gmail.com>
2025-06-16 20:40:12 +0000

    Сделал классы для базы данных

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
-from peewee import Model, CharField, ForeignKeyField, DateTimeField
+from peewee import *
 from peewee import PostgresqlDatabase
+
 from dotenv import load_dotenv
 import datetime
 import os
@@ -18,8 +19,73 @@ class BaseModel(Model):
         database = db
 
 class User(BaseModel):
-    nickname = CharField(unique=True)
+    nickname = CharField(unique=True, max_length=255)
     password = CharField()
+    role = CharField()
+
+class Food(BaseModel):
+    name = CharField(max_length=255) 
+    category = CharField(max_length=255)
+    calories = FloatField()
+    protein = FloatField()
+    fat = FloatField()
+    carbs = FloatField()
+    unit = CharField(max_length=50)
+
+class MealPlans(BaseModel):
+    user = ForeignKeyField(User, backref="mealplans")
+    date = DateField()
+    type = CharField(max_length=50) 
+    notes = TextField()
+
+class MealItems(BaseModel):
+    quantity = FloatField()
+    meal_plan = ForeignKeyField(MealPlans, backref="mealitems")  
+    food = ForeignKeyField(Food, backref="mealitems")  
+
+class UserData(BaseModel):
+    user = ForeignKeyField(User, backref="personal_data")  
+    weight = FloatField()
+    year_of_birth = IntegerField()  
+    gender = BooleanField()
+
+class LabTest(BaseModel):
+    user = ForeignKeyField(User, backref="labtests")
+    date = DateField()
+    value = FloatField()
+    unit = CharField(max_length=50) 
+    frequency = CharField(max_length=50)  
+
+class Notification(BaseModel):
+    user = ForeignKeyField(User, backref="notifications")
+    title = CharField(max_length=255)  
+
+class Reminders(BaseModel):
+    user = ForeignKeyField(User, backref="reminders") 
+    type = CharField(max_length=50)  
+    target_date = DateField()  
+    description = TextField()
+
+class Pantry(BaseModel):
+    user = ForeignKeyField(User, backref="pantries")
+    food = ForeignKeyField(Food, backref="pantries") 
+    quantity = FloatField()
+    unit = CharField(max_length=50) 
+    expiry_date = DateField()  
+
+class DietUser(BaseModel):
+    user_data = ForeignKeyField(UserData, backref="diets") 
+    diet_name = CharField(max_length=255) 
+    diet_description = TextField()  
+    is_active = BooleanField() 
+    author = CharField(max_length=255)  
+
+class SupportMessages(BaseModel):
+    user = ForeignKeyField(User, backref="support_messages")  
+    recipient_type = CharField(max_length=50)  
+    description = TextField()
+    time_date = DateTimeField() 
+
 
 class RefreshToken(BaseModel):
     user = ForeignKeyField(User, backref='tokens')
